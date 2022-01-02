fbdb877a57ce7aff6e4177ad2696c9b8c7d493fd
Elis <er.elisscherer@gmail.com>
2022-01-02 17:27:31 -0300

    refactor

--- forca.py
+++ forca.py
@@ -3,21 +3,30 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas =["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
+    erros =0
     print(letras_acertadas)
     while(not enforcou and not acertou):
         chute =input("Qual letra?")
-        chute = chute.strip();
-        index =0
-        for letra in palavra_secreta:
-            if(chute.upper()==letra.upper()):
-                letras_acertadas[index]=letra
-
-            index = index+1
+        chute = chute.strip().upper();
+        if(chute in palavra_secreta):
+            index =0
+            for letra in palavra_secreta:
+                if(chute==letra):
+                    letras_acertadas[index]=letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == len(palavra_secreta)
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+        if( acertou):
+            print("você ganhou")
+        if(enforcou):
+            print("Você Perdeu")
 
     print("Fim do jogo")
 
